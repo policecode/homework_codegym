a6ed8371396d55044b40d38535d2308baddcb6d7
Hoang-Dat <dat01655993202@gmail.com>
2024-03-11 00:11:38 +0700

    finish homework check accoutn

---
+++ regex-account.js
@@ -0,0 +1,15 @@
+function check_account(str) {
+    let regexp = /^[_a-z0-9]{6,}$/;
+    if (regexp.test(str)) {
+        return true;
+    } 
+    return false;
+}
+let accountArr = ['123abc_', '_abc123', '______', '123456', 'abcdefg', '.@', '12345', '1234_', 'abcde'];
+for (let i = 0; i < accountArr.length; i++) {
+    if (check_account(accountArr[i])) {
+        console.log(accountArr[i] + ' hợp lệ');
+    } else {
+        console.log( accountArr[i] + ' không hợp lệ');
+    }
+}
